3f299d75d7bb24510322e719977d7dd2ed5edafb
diaseu <andtheregoesmeow@gmail.com>
2021-06-03 14:04:16 -0700

    pulled into functions to fix order

--- index.js
+++ index.js
@@ -1,115 +1,76 @@
 // array of questions for user
 const questions = [
-
 ];
-
-
 
 const fs = require('fs')
 const inquirer = require('inquirer')
 
 // function to initialize program
 // const readmeGenerator = () => {
-  inquirer
-    .prompt([
-      {
-        name: "title",
-        type: 'input',
-        message: "Title of project?",
-      },
-      {
-        name: "description",
-        message: "Description of project?",
-      },
-      {
-        name: "screenshot",
-        message: "URL to screenshot of project?",
-      },
-      {
-        name: "installation",
-        message: "Installation instruction?",
-      },
-      {
-        name: "usage",
-        message: "Usage info?",
-      },
-      {
-        name: "license",
-        type: 'list',
-        message: "Test instructions?",
-        choices: ['MIT', 'Apache 2.0', 'GPLv3'],
-      },
-      {
-        name: "contributing",
-        message: "Contributing info?",
-      },
-      {
-        name: "tests",
-        message: "Test instructions?",
-      },
-    ])
-    .then((answer) => {
+inquirer
+  .prompt([
+    {
+      name: "title",
+      type: 'input',
+      message: "Title of project?",
+    },
+    {
+      name: "description",
+      message: "Description of project?",
+    },
+    {
+      name: "screenshot",
+      message: "URL to screenshot of project?",
+    },
+    {
+      name: "installation",
+      message: "Installation instruction?",
+    },
+    {
+      name: "usage",
+      message: "Usage info?",
+    },
+    {
+      name: "license",
+      type: 'list',
+      message: "Test instructions?",
+      choices: ['MIT', 'Apache 2.0', 'GPLv3'],
+    },
+    {
+      name: "contributing",
+      message: "Contributing info?",
+    },
+    {
+      name: "tests",
+      message: "Test instructions?",
+    },
+  ])
+  .then((answer) => {
 
+    function head() {
       const markdownHead = `
 <h1 align="center">${answer.title}</h1>
 <div align="center">${answer.description}</div>
 <img src="${answer.screenshot}" align="center" alt="Screenshot of Application">
 `
-
-
-      const markdownBody = `
----
-
-## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
-
----
-
-## Installation
-
-${answer.installation}
-
-## Usage
-
-${answer.usage}
-`
-      const markdownFoot = `
-## Contributing
-
-${answer.contributing}
-
-## Tests
-
-${answer.tests}
-
-## Questions
-
-${answer.tests}
-`
-function head() {
       fs.appendFile('readme.md', markdownHead, err => {
         if (err) { console.log(err) }
-        console.log('README File Created!')
+        // console.log('README File Created!')
       })
-}
-head()
+    }
+    head()
 
+
+    function license() {
       const markdownMIT = `
-[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-`
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  `
       const markdownGPL = `
-[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-`
+  [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+  `
       const markdownApache = `
-[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-`
-
-function license() {
+  [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  `
       if (answer.license === 'MIT') {
         fs.appendFile('readme.md', markdownMIT, err => {
           if (err) { console.log(err) }
@@ -123,21 +84,55 @@ function license() {
           if (err) { console.log(err) }
         })
       }
-}
-license()
+    }
+    license()
 
-function bodyFoot() {
+    function bodyFoot() {
+      const markdownBody = `
+  ---
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ---
+  
+  ## Installation
+  
+  ${answer.installation}
+  
+  ## Usage
+  
+  ${answer.usage}
+  `
+      const markdownFoot = `
+  ## Contributing
+  
+  ${answer.contributing}
+  
+  ## Tests
+  
+  ${answer.tests}
+  
+  ## Questions
+  
+  ${answer.tests}
+  `
       fs.appendFile('readme.md', markdownBody + markdownFoot, err => {
         if (err) { console.log(err) }
         console.log('README File Created!')
       })
-}
-bodyFoot()
+    }
+    bodyFoot()
 
 
-    })
-    .catch(err => console.error(err))
-}
+  })
+  .catch(err => console.error(err))
+// }
 
 // function call to initialize program
-readmeGenerator()
+// readmeGenerator()
--- readme.md
+++ readme.md
@@ -42,3 +42,42 @@ None
 ## Questions
 
 None
+
+<h1 align="center">readmeGenerate</h1>
+<div align="center">This application allows for ease of generation of README.md files for projects from console</div>
+<img src="https://image.api.playstation.com/vulcan/img/rnd/202010/1520/5liN9zIS3F0j74pareZ2Ehk0.png" align="center" alt="Screenshot of Application">
+
+  ---
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ---
+  
+  ## Installation
+  
+  Simply load console, run index.js and answer prompts.
+  
+  ## Usage
+  
+  Simply load console, run index.js and answer prompts.
+  
+  ## Contributing
+  
+  Powered by Inquirer npm
+  
+  ## Tests
+  
+  Validated by W3School
+  
+  ## Questions
+  
+  Validated by W3School
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
